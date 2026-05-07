18d450e2c905e7e8b294223c937ced6c785011c6
Aliant26 <aantosiak0111@gmail.com>
2026-05-07 22:55:12 +0200

    feat: interactive map of users

--- mapbook_lib/model.py
+++ mapbook_lib/model.py
@@ -1,7 +1,7 @@
 users = [
-    {"name": "Alicja", "location": "Bugzy Płoskie", "posts": ["Sprzedam Mercedesa",
+    {"name": "Alicja", "location": "Chorzele", "posts": ["Sprzedam Mercedesa",
                                                               "Kupię skrzynię biegów", "Ratunku! Co robić po wypadku?",
                                                               "Kto dzisiaj idzie biegać?"]},
-    {"name": "Patrycja", "location": "Krasnosielc", "posts": ["Mój kod nie działa. Pomocy!"]},
-    {"name": "Oliwia", "location": "Uciekajka", "posts": ["Czy ktoś zrobił już sprawozdanie z PPyt?"]}
+    {"name": "Patrycja", "location": "Ostrołęka", "posts": ["Mój kod nie działa. Pomocy!"]},
+    {"name": "Oliwia", "location": "Lublin", "posts": ["Czy ktoś zrobił już sprawozdanie z PPyt?"]}
 ]
